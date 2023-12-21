e04760814907069cf461c82fd97d9c71b5925ae9
Ignacio Castillejo Munoz <igncasti@c1r13s4.42malaga.com>
2023-12-21 20:35:34 +0100

    strim2

--- libft/ft_strtrim2.c
+++ libft/ft_strtrim2.c
@@ -1,22 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_strtrim2.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: igncasti <igncasti@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/08 20:03:46 by igncasti          #+#    #+#             */
-/*   Updated: 2023/12/19 20:54:22 by igncasti         ###   ########.fr       */
+/*   Created: 2023/12/21 18:14:32 by igncasti          #+#    #+#             */
+/*   Updated: 2023/12/21 20:33:15 by igncasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
-#include <string.h>
 
+char	*ft_strtrim2(char const *s1, char const *set)
+{
+	int			found_start;
+	int			found_end;
+	char		*res;
+
+	found_start = 0;
+	found_end = ft_strlen(s1) - 1;
+	while (ft_strchr(set, s1[found_start]) && s1[found_start])
+		found_start++;
+	while (ft_strchr(set, s1[found_end]))
+		found_end--;
+	res = ft_substr(s1, found_start, found_end - found_start + 1);
+	return (res);
+}
 
 int	main(void)
 {
-	char	*my_str = "hola";
-	printf("%d", ft_strlen(my_str));
+	printf("%s", ft_strtrim2(" h ola ", " n"));
 }

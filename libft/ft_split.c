d91d766f73c41897a0ddfd63c2cd19bd3b05c7a6
Ignacio Castillejo Munoz <igncasti@c3r1s3.42malaga.com>
2024-01-15 21:57:53 +0100

    split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -13,24 +13,88 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/*
-char	**ft_split(char const *s, char c);
+int	ft_strlen(const char *str)
 {
-	char	**res;
+	int	i;
 
-	res[0]
-}*/
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	count_words(const char *s, char c)
+{
+	int	i;
+	int	w;
+
+	i = 0;
+	w = 0;
+	while (s[i] == c)
+		i++;
+	if (s[0] != c && s[0])
+		w++;
+	while (s[i])
+	{
+		if (s[i] == c && s[i + 1] != c)
+			w++;
+		i++;
+	}
+	return (w);
+}
+
+char	*get_word(char *s, int j, char c)
+{
+	char		*str;
+	int			i;
+	int			k;
+	int			l;
+
+	i = 0;
+	k = j - 1;
+	l = 0;
+	while (s[++k] == c)
+		j++;
+	while (s[k] != c && s[k])
+	{
+		i++;
+		k++;
+	}
+	str = malloc(i + 1);
+	while (s[j] != c && s[j])
+	{
+		str[l] = s[j];
+		l++;
+		j++;
+	}
+	str[l] = '\0';
+	return (str);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	int			w;
+	int			i;
+	char		**res;
+	int			j;
+
+	w = count_words((char *)s, c);
+	res = (char **)malloc(sizeof(char *) * w);
+	i = 0;
+	j = 0;
+	while (i < w)
+	{
+		res[i] = get_word((char *)s, j, c);
+		j = j + ft_strlen(res[i]);
+		i++;
+	}
+	return ((char **)res);
+}
 
 int	main(void)
 {
 	char	**res;
 
-	res = (char **)malloc(sizeof(char*) * 2);
-	if (!res)
-		return (0);
-	res[0] = "hola";
-	res[1] = "adios";
-	/*printf("%s", ft_split("hola me llamo Nacho", ' ')[0]);
-	*/
-	printf("%s", res[0]);
+	res = ft_split("hola me llamo", ' ');
+	printf("%s", res[2]);
 }
